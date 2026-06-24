cedccc047f7864fb704cafc2a11b81205991c10b
pedroabreuu <99131505+pedroabreuu@users.noreply.github.com>
2026-06-23 23:14:21 -0300

    fix: adicionando type hints

--- command.py
+++ command.py
@@ -6,61 +6,61 @@ class Comando(ABC):
         pass
 
 class Pessoa:
-    def __init__(self, pessoaID, nome):
+    def __init__(self, pessoaID: int, nome: str) -> None:
         self.pessoaID = pessoaID
         self.nome = nome
 
 # receptor
 class BancoPessoas:
-    def __init__(self):
+    def __init__(self) -> None:
         self.pessoas = {}
 
-    def new(self, pessoaID, nome):
+    def new(self, pessoaID: int, nome: str) -> None:
         if pessoaID in self.pessoas:
             raise ValueError("ja existe uma pessoa com este id")
         self.pessoas[pessoaID] = Pessoa(pessoaID, nome)
 
-    def delete(self, pessoaID):
+    def delete(self, pessoaID: int) -> None:
         if pessoaID in self.pessoas:
             self.pessoas.pop(pessoaID)
         else:
             raise ValueError("id nao encontrado")
 
-    def get(self, pessoaID):
+    def get(self, pessoaID: int) -> Pessoa:
         if pessoaID not in self.pessoas:
             raise ValueError("pessoa nao encontrada")
         
         return self.pessoas[pessoaID]
     
-    def all(self):
+    def all(self) -> list[Pessoa]:
         return list(self.pessoas.values())
 
 class NewComando(Comando):
-    def __init__(self, banco):
+    def __init__(self, banco: BancoPessoas) -> None:
         self.banco = banco
 
-    def executar(self, args):
+    def executar(self, args) -> None:
         pass
 
 class DeleteComando(Comando):
-    def __init__(self, banco):
+    def __init__(self, banco) -> None:
         self.banco = banco
 
-    def executar(self, args):
+    def executar(self, args) -> None:
         pass
 
 class GetComando(Comando):
-    def __init__(self, banco):
+    def __init__(self, banco) -> None:
         self.banco = banco
 
-    def executar(self, args):
+    def executar(self, args) -> None:
         pass
 
 class AllComando(Comando):
-    def __init__(self, banco):
+    def __init__(self, banco) -> None:
         self.banco = banco
 
-    def executar(self, args):
+    def executar(self, args) -> None:
         pass
 
 class Executor:
